3e45b04621721f90ae219e6491cf1e0cd6d9a5ff
sky3429 <riyarouniyar102@gmail.com>
2024-11-18 20:19:46 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -21,7 +21,7 @@ if (isset($_POST['submit'])) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <title>College Adnission Management System || Contact Us</title>
+  <title>College Admission System || Contact Us</title>
   <!-- web fonts -->
   <!-- Favicons -->
   <link rel="icon" href="./assets/images/favicon/favicon.ico" type="image/x-icon" />
